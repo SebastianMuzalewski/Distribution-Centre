ff658a01c81e1508721c311bdf4a4c68ae785a09
Sebastian Paul Muzalewski <113144733+SebastianMuzalewski@users.noreply.github.com>
2023-07-27 02:41:41 -0400

    🎉 First Commit form Seb 🎉 - 🟩 Added a left side navbar - 🟩 Added Home and About page - 🟩 Added GitHub page link in footer

---
+++ src/main/java/com/deez/distribution_center/controller/AboutController.java
@@ -0,0 +1,15 @@
+package com.deez.distribution_center.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/about")
+public class AboutController {
+    @GetMapping
+    public String home(Model model) {
+        return "about";
+    }
+}
