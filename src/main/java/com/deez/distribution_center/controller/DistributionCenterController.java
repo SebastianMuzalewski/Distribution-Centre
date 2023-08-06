cd7d9deac8c31e0d4187da286d59f04a458adf9b
Sebastian Paul Muzalewski <113144733+SebastianMuzalewski@users.noreply.github.com>
2023-08-05 22:44:18 -0400

    Forgot import statements

--- src/main/java/com/deez/distribution_center/controller/DistributionCenterController.java
+++ src/main/java/com/deez/distribution_center/controller/DistributionCenterController.java
@@ -1,5 +1,7 @@
 package com.deez.distribution_center.controller;
 
+import com.deez.distribution_center.model.DistributionCenter;
+import com.deez.distribution_center.model.Item;
 import com.deez.distribution_center.model.dto.DBCentersDto;
 import com.deez.distribution_center.repository.DBCRepository;
 import com.deez.distribution_center.repository.DBCRepositoryPaginated;
@@ -10,6 +12,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Optional;
 
 @Controller
