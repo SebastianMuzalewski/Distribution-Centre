fee97aae1a51a8bed724e51820b2d9bb3e36cc17
Sebastian Paul Muzalewski <113144733+SebastianMuzalewski@users.noreply.github.com>
2023-08-05 22:23:26 -0400

    Icon/Styling Update + Rest Template + Home Page - 🟩 | Added New profile icon along with add_item & center icon - 🟩 | Added Rest Template method in WebConfig - 🟩 | Added content within the home page - 🟩 | Adjusted styling within distribution-centers & details page

--- src/main/java/com/deez/distribution_center/config/WebConfig.java
+++ src/main/java/com/deez/distribution_center/config/WebConfig.java
@@ -3,6 +3,8 @@ package com.deez.distribution_center.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
@@ -10,5 +12,9 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/authentication/login");
+    }
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
     }
 }
